d673b39b8910b5a8b13cc8efc0c54616b04c4371
sleeper_ok <sleeeper.ok@gmail.com>
2021-11-20 15:27:15 +0300

    Create index.ts

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import dotenv from 'dotenv'
+import Werror from './lib/errors'
+import initBot from './bot/index'
+
+dotenv.config()
+
+const token = process.env.BOT_TOKEN;
+if (!token) {
+  throw new Werror('Not bot token specified');
+}
+
+(async() => {
+  await initBot(token)
+})()
