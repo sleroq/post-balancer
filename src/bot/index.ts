420f0024e03aab85ddc05b259f0970146c62c7a1
sleeper_ok <sleeeper.ok@gmail.com>
2021-11-20 18:43:01 +0300

    Fixed crashing on errors

--- src/bot/index.ts
+++ src/bot/index.ts
@@ -3,6 +3,10 @@ import { Bot } from 'grammy'
 export default async function initBot(token: string) {
 	const bot = new Bot(token)
 
+	bot.catch((error) => {
+		console.error(error)
+	})
+
 	bot.on('message', (ctx) => ctx.reply('Hi there!'))
 
 	return bot
