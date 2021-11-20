17cb5c37d42f7b351e955bf005831229ad743124
sleeper_ok <sleeeper.ok@gmail.com>
2021-11-20 18:28:20 +0300

    Update index.ts
    Removed unnecessary await

--- src/bot/index.ts
+++ src/bot/index.ts
@@ -3,7 +3,7 @@ import { Bot } from 'grammy'
 export default async function initBot(token: string) {
 	const bot = new Bot(token)
 
-	await bot.on('message', (ctx) => ctx.reply('Hi there!'))
+	bot.on('message', (ctx) => ctx.reply('Hi there!'))
 
-	await bot.start()
+	return bot
 }
