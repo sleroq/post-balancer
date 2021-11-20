e5131878298cbb50659bc857d9be1023e046e097
sleeper_ok <sleeeper.ok@gmail.com>
2021-11-20 22:40:24 +0300

    Created connectToMongo function

---
+++ src/lib/database/connect.ts
@@ -0,0 +1,13 @@
+import Werror from '../errors'
+
+import mongoose from 'mongoose'
+
+export default async function connectToMongo(databaseUrl: string) {
+	try {
+		await mongoose.connect(databaseUrl)
+	} catch (error) {
+		throw new Werror(error, 'Connecting to MongoDB')
+	}
+
+	console.log('Mongoose is connected!')
+}
