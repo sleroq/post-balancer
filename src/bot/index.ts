5423f5eb32a1c23e3218e9f5184347e661ee44a6
sleeper_ok <sleeeper.ok@gmail.com>
2021-11-20 22:43:28 +0300

    Added the start command which saves the user

--- src/bot/index.ts
+++ src/bot/index.ts
@@ -1,13 +1,26 @@
-import { Bot } from 'grammy'
+import { Bot, Context } from 'grammy'
+import { saveNewUser } from '../lib/database/queries'
+import Werror from '../lib/errors'
 
 export default async function initBot(token: string) {
 	const bot = new Bot(token)
 
-	bot.catch((error) => {
-		console.error(error)
-	})
+	bot.catch((error) => { console.error(error) })
 
+	bot.command('start', async ctx => await handleStart(ctx))
 	bot.on('message', (ctx) => ctx.reply('Hi there!'))
 
 	return bot
+}
+
+async function handleStart(ctx: Context) {
+	if (!ctx.from) return
+
+	try {
+		await saveNewUser(ctx.from)
+	} catch (error) {
+		throw new Werror(error, 'Saving user')
+	}
+
+	await ctx.reply('Hi there!')
 }
