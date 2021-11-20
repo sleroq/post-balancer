b7ded1e320769bd9509294f865317e680709e0ba
sleeper_ok <sleeeper.ok@gmail.com>
2021-11-20 18:30:17 +0300

    Added setWebhook function

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv'
 import Werror from './lib/errors'
 import initBot from './bot/index'
+import setWebhook from './server'
 
 dotenv.config()
 
@@ -9,6 +10,29 @@ if (!token) {
 	throw new Werror('Not bot token specified')
 }
 
-(async() => {
-	await initBot(token)
+void (async() => {
+	let bot
+	try {
+		bot = await initBot(token)
+	} catch (error) {
+		throw new Werror(error, 'Initializing the bot')
+	}
+	
+	const urlForWebhook = getUrlForWebhook()
+	if (urlForWebhook) {
+		try {
+			await setWebhook(bot, urlForWebhook)
+		} catch (error) {
+			throw new Werror(error, 'Starting with webhook')
+		}
+	} else {
+		await bot.start({
+			drop_pending_updates: true
+		})
+	}
+
 })()
+
+function getUrlForWebhook(): string | null {
+	throw new Error('Function not implemented.')
+}
