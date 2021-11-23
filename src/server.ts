e69bea816266ef91c16325d9dcf6e376f193e8b9
sleeper_ok <sleeeper.ok@gmail.com>
2021-11-23 23:09:37 +0300

    Fixed comments

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,7 @@ export default async function setWebhook(bot: Bot<SessionContext>, urlForWebhook
 	const server = express()
 	server.use(express.json())
 
-	// 3. Register a handler for the bot
+	// Register a handler for the bot
 	// eslint-disable-next-line @typescript-eslint/no-misused-promises
 	server.post('/webhook', webhookCallback(bot, 'express'))
 
@@ -14,7 +14,7 @@ export default async function setWebhook(bot: Bot<SessionContext>, urlForWebhook
 
 	const webhookURL = new URL('/webhook', urlForWebhook)
 
-	// 4. Set webhook for handler in Bot API
+	// Set webhook for handler in Bot API
 	await bot.api.setWebhook(webhookURL.toString(), {
 		drop_pending_updates: true
 	})
